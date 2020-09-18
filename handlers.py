2f923eadcfef8237510c7444efd039bb10928dab
Merijn <merijnkruithof@hotmail.cm>
2020-09-18 17:02:44 +0200

    Using chain of responsibility pattern

--- handlers.py
+++ handlers.py
@@ -1,5 +1,39 @@
-class CheckIfFurniExists:
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Handler(ABC):
+    @abstractmethod
+    def set_next(self, handler: Handler) -> Handler:
+        pass
+
+    @abstractmethod
+    def handle(self, data):
+        pass
+
+
+class AbstractHandler(Handler):
+    _next: Handler = None
+
+    @abstractmethod
     def handle(self, data) -> None:
-        print("validator got")
-        print(data['url'])
-        print(data['content'])
+        if self._next:
+            return self._next.handle(data)
+
+    def set_next(self, handler: Handler) -> Handler:
+        self._next = handler
+
+        return handler
+
+
+class FurniExistsHandler(AbstractHandler):
+    def handle(self, data) -> None:
+        print("validator got", data['url'])
+        print("file does not exist, continue...")
+
+        return super().handle(data)
+
+
+class GetFurniMetadataHandler(AbstractHandler):
+    def handle(self, data) -> None:
+        print("getting metadata of", data['url'])
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from interceptors import FurnitureInterceptor
-from handlers import CheckIfFurniExists
+from handlers import FurniExistsHandler, GetFurniMetadataHandler
 from dispatcher import Dispatcher
 from mitmproxy import ctx
 
 # Kill noise by setting verbosity to 0 (flow_detail)
 ctx.options.flow_detail = 0
 
-listeners = [CheckIfFurniExists()]
+furni_exists_handler = FurniExistsHandler()
+furni_metadata_handler = GetFurniMetadataHandler()
+
+furni_exists_handler.set_next(furni_metadata_handler)
+
+listeners = [furni_exists_handler]
 dispatcher = Dispatcher(listeners)
 
 addons = {
